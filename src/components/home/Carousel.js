a1fc782e879890065dc86218e42d05fe2ae5e73f
Prachi kesharwani <prachikesharwani03@gmail.com>
2024-09-21 21:27:17 +0530

    initial commit

---
+++ src/components/home/Carousel.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const imageProp = ["/pizza.png", "/Burger.png", "/fries.png"];
+
+function CarouselComponent() {
+  return (
+    <Carousel
+      autoPlay
+      navButtonsAlwaysVisible
+      infiniteLoop
+      showStatus={false}
+      emulateTouch
+      showThumbs={false}
+    >
+      {imageProp.map((image, index) => {
+        return (
+          <div
+            key={index}
+            style={{ maxHeight: "36rem" }}
+            className="object-center brightness-70"
+          >
+            <img
+              src={image}
+              alt="pizza"
+            />
+          </div>
+        );
+      })}
+    </Carousel>
+  );
+}
+
+export default CarouselComponent;
